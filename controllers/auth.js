cbcf709d06f3119704f891d6c2b92fc8fcbe6743
Mohammad Sakr <m.attia@moto-soft.com>
2019-05-15 03:44:41 +0200

    Adding validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const crypto = require("crypto");
 
 const bcrybt = require("bcryptjs");
+const { validationResult } = require("express-validator/check");
 
 const User = require("../models/user");
 
@@ -69,26 +70,28 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash("Error", "Email used already");
-        res.redirect("/signup");
-      }
-      return bcrybt
-        .hash(password, 12)
-        .then(hashedPassword => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] }
-          });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg
+    });
+  }
+  return bcrybt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] }
+      });
 
-          return user.save();
-        })
-        .then(result => {
-          res.redirect("/login");
-        });
+      return user.save();
+    })
+    .then(result => {
+      res.redirect("/login");
     })
     .catch(err => console.log(err));
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { check, body } = require("express-validator/check");
 
 const authController = require("../controllers/auth");
+const User = require("../models/user");
 
 const router = express.Router();
 
@@ -12,7 +14,36 @@ router.post("/login", authController.postLogin);
 
 router.post("/logout", authController.postLogout);
 
-router.post("/signup", authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Enter a valid mail")
+      .custom((value, { req }) => {
+        // if (value === "test@test.com") {
+        //   throw new Error("This email is forbidden");
+        // }
+        // return true;
+        returnUser.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject("Email used already");
+          }
+        });
+      }),
+    body("password")
+      .isLength({ min: 5 })
+      .withMessage("Enter password with at least 5 numbers")
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("passwords have to match");
+      }
+      return true;
+    })
+  ],
+  authController.postSignup
+);
 
 router.get("/reset", authController.getReset);
 
